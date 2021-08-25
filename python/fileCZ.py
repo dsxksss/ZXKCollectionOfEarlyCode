a69e44cadcc4512c8e5e5f3af24a7ed394137c27
dsxksss <2546650292@qq.com>
2021-08-25 17:22:48 +0800

    文件操作
    增加了文件内容的读写操作

--- python/fileCZ.py
+++ python/fileCZ.py
@@ -1,5 +1,5 @@
 #py的文件操作
-from pathlib import Path  #导入处理文件的类
+from pathlib import Path  #导入文件搜索功能类
 
 path = Path("newfiles/name.txt")
 #print(path.exists())  #判断文件或文件夹是否存在,return(True/flase)
@@ -8,3 +8,17 @@ path = Path("newfiles/name.txt")
 # glob函数记得(不能设置当前Path)
 for sum in path.glob('*.py'):  #C1:搜索当前目录下的.py文件
     print(sum)  #打印当前目录下.py后缀的文件名字,也可以'*'搜索当前目录下的全部文件名
+
+#文件读写入
+
+#文件写
+file_name = 'tist.txt'  #文件相对路径
+file_w = open(file_name, 'a')  #w表示:有内容则覆盖,a表示:不删除内容在末尾追加
+file_w.write("hola this is test \n")
+
+#文件读
+file_r = open(file_name)
+lien = file_r.readlines()  #多次读取一行数据
+lien2 = file_r.read()
+print(lien, '\n')
+print(lien2)
