7ab1cb2a0f4dd4d32282216a94a223f8fbffe362
dsxksss <2546650292@qq.com>
2021-09-08 23:56:21 +0800

    JavaScript基本操作

---
+++ JavaScript/BLLX.js
@@ -0,0 +1,12 @@
+//变量的创建
+//ES6出来之前我们一般用var来创建变量,但是是有缺陷的,最好使用let来
+//let表示这是一个变量,它的类型是auto的,按内容来标识其type
+//建议使用驼峰命名法,变量名对大小写会敏感,支持一行多定义多赋值
+let name1="Jack";
+console.log(name1);
+
+
+//常量的创建
+//和C、C++语言一样,使用const表示此内容不可变
+const name2="Bob";
+console.log(name2);
---
+++ JavaScript/index.js
@@ -0,0 +1,4 @@
+//javaScript代码
+      //单双引号都行
+      console.log("你好！"); //向屏幕output些字符串
+
