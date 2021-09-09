53660a218c171599a5a6f091f364ac1b02a327e1
dsxksss <2546650292@qq.com>
2021-09-09 22:44:09 +0800

    基本类型及动态类型解释

---
+++ JavaScript/LX.js
@@ -0,0 +1,10 @@
+//基本类型
+
+let bl=""//字符串String
+let b2=20//数字Number,javaScript语言只有一个数字类型,不区别小数整数等
+let b3=true //布尔值Boolean
+let b4=undefined//未定义undefined,不太常用
+let b5=null//空值object类型,一般用于清空值内容
+
+//javaScript是动态类型语言,在程序运行时可以更改变量类型
+//引用类型
