f25d8a5160796828d9e547906a7f6ac30f7b2462
dsxksss <2546650292@qq.com>
2021-09-12 18:11:03 +0800

    函数定义

---
+++ JavaScript/function.js
@@ -0,0 +1,16 @@
+//定义函数,语法:
+//function关键字 + name(){}
+function fcti(){
+    //TODO THING
+}
+//这里可以使用模板来简化代码
+function callName(name){
+    console.log('Hello '+name);
+}
+
+function square(number){
+    return number*number;
+}
+
+callName('Jack');
+console.log(square(5));
