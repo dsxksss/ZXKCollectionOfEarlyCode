cf13d0f4861877b5fe9c3d5cae0c2ae5c09d291e
dsxksss <2546650292@qq.com>
2021-09-23 17:44:22 +0800

    部分笔记增加或修改解答意思

--- JavaScript/LX.js
+++ JavaScript/LX.js
@@ -1,10 +1,15 @@
 //基本类型
+//var和let作用差不多，但创建出的变量作用域不一样，var不局限于代码块
+//而let 和 const 则会被代码块影响作用域
 
-let bl=""//字符串String
-let b2=20//数字Number,javaScript语言只有一个数字类型,不区别小数整数等
-let b3=true //布尔值Boolean
-let b4=undefined//未定义undefined,不太常用
-let b5=null//空值object类型,一般用于清空值内容
+let bl="";//字符串String
+let b2=20;//数字Number,javaScript语言只有一个数字类型,不区别小数整数等
+let b3=true;//布尔值Boolean
+let b4=undefined;//未定义undefined,不太常用
+let b5=null;//空值object类型,一般用于清空值内容
+
+//不能更改内容
+const c1="ddd";
 
 //javaScript是动态类型语言,在程序运行时可以更改变量类型
 //引用类型
--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,4 +1,26 @@
-//javaScript代码
+//javaScript代码区分大小写
+//javaScript语法规范就是在代码末尾加上英文;号表示此行代码结束，但是也可以忽略不写
+//因为解析器引擎会自动在每段代码末尾加上英文分号，尽管可以省略，但在一些特定语句会导致错误，
+//所以还是不建议省略，javaScript不允许重复声明已有变量
+
       //单双引号都行
       console.log("你好！"); //向屏幕output些字符串
+      console.info("info");//提示信息
+      console.warn("warn");//警告信息
+      console.error("error");//错误信息
+      console.debug("debug\n");//调试信息
+//占位符略解
+      const obj={//一个对象的创建，在objects.js文件里有详细解释，这里跳过不赘述
+            name:'Jack',
+            age:30
+      };
+      
+      //实测部分可以互用，javaScript语言不是强类型语言，可能是因为其动态性质导致
+      console.log('%o',obj);//%o:javaScript对象、整数、字符串、以及json数据
+      console.log('三个整数%d%d%i',2,5,231);//%d或%i:整数
+      console.log('%s','我是字符串');//%s:字符串
+      console.log('浮点%f数',2.123);//%f:浮点数
 
+      //js也支持颜色输出
+      console.log("\033[1;31mfuck you\033[32m");
+      //console.clear();//清屏功能
